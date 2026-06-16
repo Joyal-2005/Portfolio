a6f3ac69ba339b07ef5724dd0d4b67f63723706b
Joyal-2005 <joyalaugustin17@gmail.com>
2026-06-16 20:59:57 +0530

    protfoli

---
+++ script.js
@@ -0,0 +1,21 @@
+const text =
+"ServiceNow Certified System Administrator";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing").innerHTML +=
+text.charAt(i);
+
+i++;
+
+setTimeout(typing,80);
+
+}
+
+}
+
+typing();
